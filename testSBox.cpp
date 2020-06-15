30d23b3e22979660c356d4ae36564bcf9a922b92
Samuel <sam.arzur@gmail.com>
2020-06-15 17:41:00 +0200

    Test sBox

--- testSBox.cpp
+++ testSBox.cpp
@@ -71,12 +71,19 @@ static void test_sBox()
 		}
 	};
 
-	int matrice_test[4][16] = {
-                         			{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
-                         			{0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
-                         			{4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
-                         			{15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}
-                         		};
+    int** matrice_test;
+    matrice_test = new int* [4];
+
+    for (int i = 0; i < 4; i++)
+    	{
+    		matrice_test[i] = new int[16];
+
+    		// Copie de la valeur de la matrice en param
+    		for (int j = 0; j < 16; j++)
+    		{
+    			matrice_test[i][j] = sboxes[0][i][j];
+    		}
+    	}
 
     sBox sbox = sBox(matrice_test);
     Sequence seq_entree = Sequence(6);
